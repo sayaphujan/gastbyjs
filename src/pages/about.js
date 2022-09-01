c3e6fe1aeeede3cfb75187f75ed9cb9e8bffa326
sayaphujan <shinta.setiawati@gmail.com>
2022-09-01 08:34:02 +0700

    Finished Tutorial Part 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,7 @@
-// Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
-// Step 2: Define your component
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
@@ -11,14 +10,6 @@ const AboutPage = () => {
   )
 }
 
-// Step 3: Export your component
-//export const Head = () => <title>About Me</title>
-
-export const Head = () => (
-  <>
-    <title>About Me</title>
-    <meta name="about" content="About Me" />
-  </>
-)
+export const Head = () => <Seo title="About Me" />
 
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,20 @@
-// Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
 
-// Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="../images/clifford.jpg"
+      />
     </Layout>
   )
 }
 
+export const Head = () => <Seo title="Home Page" />
 
-// You'll learn about this in the next task, just copy it for now
-//export const Head = () => <title>Home Page</title>
-
-export const Head = () => (
-  <>
-    <title>Home Page</title>
-    <meta name="home" content="Home Page" />
-  </>
-)
-
-// Step 3: Export your component
 export default IndexPage
